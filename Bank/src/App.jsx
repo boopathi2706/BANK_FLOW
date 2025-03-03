840f0265d8eb04a736390b3300837385d9df0b5e
boopathi2706 <boopathi.v2023cse@sece.ac.in>
2025-03-03 08:51:04 +0530

    complete front end

--- Bank/src/App.jsx
+++ Bank/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import './App.css'
 import Login from './components/Login';
 import Main from './components/Main';
+// import VoiceToText from './components/VoiceToText';
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Login/>}></Route>
       <Route path='/main' element={<Main />}></Route>
+      {/* <Route path='/sample' element={<VoiceToText />}></Route> */}
     </Routes>
     </BrowserRouter>
     {/* <Main /> */}
